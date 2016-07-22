094ede8132b6e06af5660434b7c8e887f8a7faae
Cronán Gogarty <cronangogarty@outlook.com>
2016-07-22 03:01:27 -0700

    Moved site to ASP.NET MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Razor;
+
+namespace CronanGogarty.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult about()
+        {
+            return View();
+        }
+    }
+}
